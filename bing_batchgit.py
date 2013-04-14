c3faea498a615786a546353471bd2ba5ae39af6d
BingWang <toaya.kase@gmail.com>
2013-04-13 21:15:45 -0500

    add start/end function for bing_batchgit

--- bing_batchgit.py
+++ bing_batchgit.py
@@ -4,18 +4,23 @@
 # Email: toaya.kase@gmail.com
 # Copyleft Bing Wang
 # LICENCES: GPL v3.0
-# version: 0.1
+# version: 0.11
 
 
 __docformat__ = "epytext en"
 '''
 This program do git for all git repository under current path.
-In this version it has:
+In version 0.1 it has:
 
 # for pull request
 bing_batchgit.py pull
 # for push request
 bing_batchgit.py push
+
+new in version 0.11
+
+#same as pull/push
+bing_batchgit.py start/end
 '''
 
 import os,sys
@@ -24,14 +29,14 @@ def print_help():
     print "batch git for all git repository under current path"
     print "USAGE\n"
     print "To push all repository under current path"
-    print "./bing_batchgit.py push\n"
+    print "./bing_batchgit.py push/end\n"
     print "To pull all repository under current path"
-    print "./bing_batchgit.py pull\n"
+    print "./bing_batchgit.py pull/start\n"
 
 if len(sys.argv) != 2:
     print_help()
 
-elif sys.argv[1] == "pull":
+elif sys.argv[1] in ["pull","start"]:
     current_dir = os.getcwd() + "/"
     for item in os.listdir(current_dir):
         if os.path.isdir(current_dir+item):
@@ -40,7 +45,7 @@ elif sys.argv[1] == "pull":
                 print "git pull %s"%(item)
                 os.system(cmd)
 
-elif sys.argv[1] == "push":
+elif sys.argv[1] == ["push","end"]:
     current_dir = os.getcwd() + "/"
     for item in os.listdir(current_dir):
         if os.path.isdir(current_dir+item):
