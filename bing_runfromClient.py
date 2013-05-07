060120f2edfbeec6cdffa614ab040f0c37f037ab
Bing Wang <toaya.kase@gmail.com>
2013-05-07 12:35:29 -0500

    update bing_runfromClient

--- bing_runfromClient.py
+++ bing_runfromClient.py
@@ -3,13 +3,27 @@
 
 #this program helps run command on LabServer through your macprobook Client
 
-#IMPORT: Acturally it just replace "/Volumes/hittingerlab/" to "~" and run the command
+#IMPORTANT: Acturally it just replace "/Volumes/hittingerlab/" to "~/" and run the command
 
 import os,sys
+
+def print_usage():
+    print "\nUsage:\n"
+    print "./bing_runfromClient.py command\n"
+    print "This program will print a command which can run from your ssh client."
+    print "Acturally what this program do it just replace \"/Volumes/hittingerlab/\" to \"~/\"\n"
+
+if len(sys.argv) == 1:
+    print_usage()
+    sys.exit()
+
 cmd = " ".join(sys.argv[1:]).replace("/Volumes/hittingerlab/","~/")
 print cmd
+
+'''
 if raw_input("\nAre you sure to run this cmd? (y/n)\n") == "y":
     os.system(cmd)
 else:
     print "exit"
     sys.exit()
+'''
